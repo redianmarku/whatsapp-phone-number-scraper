b46a0a59aec0b13ca1398771a6f60228d7104628
MOHAMED ELNABARAWI <41297184+mohammednabarawy@users.noreply.github.com>
2023-12-21 17:14:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ driver.get('https://web.whatsapp.com/')
 # Wait for the user to scan the QR code and log in to WhatsApp
 input('Press Enter after scanning QR code and logging in to WhatsApp')
 
-# Wait for the chat to load and find the search box
-search_box = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located(
-        (By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/div/div[1]/div/div[2]/div[2]/div/div[1]/p'))
-)
-
-# Search for the group by name
-group_name = 'سوق الرياض'  # Replace with the name of the group you want to scrape
-search_box.send_keys(group_name)
-search_box.send_keys(Keys.ENTER)
-
 # Wait for the group chat to load and find the member elements
 member_elements = WebDriverWait(driver, 10).until(
     EC.presence_of_all_elements_located(
@@ -44,4 +33,4 @@ for i, phone_number in enumerate(phone_numbers, start=1):
     worksheet.cell(row=i, column=1, value=phone_number)
 
 # Save the workbook
-workbook.save('C:\\Users\\user\\Desktop\\whatsapp scrape\\member_info.xlsx')
+workbook.save('member_info.xlsx')
